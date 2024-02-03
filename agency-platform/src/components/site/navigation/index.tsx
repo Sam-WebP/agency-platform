6fd87530c3ed99b218ee60d41366287fe83b7c44
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-02-03 21:32:51 +1100

    Fixed the hydration warning

--- agency-platform/src/components/site/navigation/index.tsx
+++ agency-platform/src/components/site/navigation/index.tsx
@@ -1,5 +1,8 @@
+import { ModeToggle } from '@/components/global/mode-toggle'
+import { UserButton } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/server'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {
@@ -16,6 +19,24 @@ const Navigation = ({ user }: Props) => {
                 height={40}
                 alt='plura'
             />
+            <span className='text-x1 font-bold'>EDMON</span>
+        </aside>
+        <nav className='hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]'>
+          <ul className='flex items-center justify-center gap-8'>
+            <Link href={'#'}>Pricing</Link>
+            <Link href={'#'}>About</Link>
+            <Link href={'#'}>Documentation</Link>
+            <Link href={'#'}>Features</Link>
+          </ul>
+        </nav>
+        <aside className='flex gap-2 items-center'>
+          <Link
+          href={'/agency'}
+          className='bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80'>
+            Login
+          </Link>
+          <UserButton />
+          <ModeToggle />
         </aside>
     </div>
   )
