5782ac19ee835ee18d13ff40137e0b1091516ff7
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-02-04 19:57:46 +1100

    I updated the landing page to run better on different screen sizes

--- agency-platform/src/app/site/page.tsx
+++ agency-platform/src/app/site/page.tsx
@@ -23,17 +23,17 @@ export default async function Home() {
         <div className="flex justify-center items-center relative md:mt-[-70px]">
 
           <Image 
-            src={'/assets/preview.png'}
+            src='/assets/preview.png'
             alt='banner image'
             height={1200}
             width={1200}
             className='rounded-tl-2xl rounded-tr-2xl border-2 border-muted'
           />
-          <div className='bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'></div>
+          <div className='bottom-0 top-[20%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'></div>
         </div>
           
       </section>
-      <section className='flex justify-center items-center flex-col gap-4 md:mt-20'>
+      <section className='flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-70px] z-20 relative'>
         <h2 className='text-4xl text-center'> Choose what fits you right</h2>
         <p className='text-muted-foreground text-center'>
           Our straightforward pricing plans are tailored to meet your needs. if {" you're"} not <br />
@@ -42,7 +42,8 @@ export default async function Home() {
         <div className='flex justify-center gap-4 flex-wrap mt-6'>
           {pricingCards.map((card) => (
             <Card 
-              key={card.title} 
+              key={card.title}
+              //WIP: Wire up free product from stripe
               className={clsx('w-[300px] flex flex-col justify-between', 
                 {'border-2 border-primary': card.title === 'Unlimited Saas',
               })}
--- agency-platform/src/components/global/mode-toggle.tsx
+++ agency-platform/src/components/global/mode-toggle.tsx
@@ -4,13 +4,13 @@ import * as React from "react"
 
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/src/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/src/components/ui/dropdown-menu"
 import { MoonIcon, SunIcon } from "lucide-react"
 
 export function ModeToggle() {
