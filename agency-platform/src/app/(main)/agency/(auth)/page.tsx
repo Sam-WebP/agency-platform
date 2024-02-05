009e30dbb0769406f962d35a0439537b843fc1f2
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-02-05 18:17:14 +1100

    Adding Prisma ORM and a couple of queries

--- agency-platform/src/app/(main)/agency/(auth)/page.tsx
+++ agency-platform/src/app/(main)/agency/(auth)/page.tsx
@@ -1,9 +1,15 @@
+import { getAuthUserDetails } from '@/src/lib/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Page = () => {
-  return (
-    <div>Agency</div>
-  )
+const Page = async () => {
+  const authUser = await currentUser()
+  if (!authUser) return redirect('/sign-in')
+
+  //get users details
+  const user = await getAuthUserDetails()
+  return <div>Agency</div>
 }
 
 export default Page
