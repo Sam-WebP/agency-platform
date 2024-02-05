3ef6facfe603781db30ca708398f9efb9f6f6840
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-02-05 23:13:23 +1100

    Added content, routes and a form to create an agency

--- agency-platform/src/lib/queries.ts
+++ agency-platform/src/lib/queries.ts
@@ -3,6 +3,7 @@
 import { clerkClient, currentUser } from "@clerk/nextjs"
 import { db } from "./db"
 import { redirect } from "next/navigation"
+import { Agency } from "@prisma/client"
 
 export const getAuthUserDetails = async () => {
   const user = await currentUser()
@@ -125,13 +126,13 @@ export const createTeamUser = async (agencyId: string, user: User) => {
 export const verifyAndAcceptInvitation = async () => {
   const user = await currentUser()
   if (!user) return redirect('/sign-in')
+
   const invitationExists = await db.invitation.findUnique({
     where: {
       email: user.emailAddresses[0].emailAddress,
       status: 'PENDING',
     },
   })
-
 
   if (invitationExists) {
     const userDetails = await createTeamUser(invitationExists.agencyId, {
@@ -171,4 +172,15 @@ export const verifyAndAcceptInvitation = async () => {
     })
     return agency ? agency.agencyId : null
   }
+}
+
+export const updateAgencyDetails = async (
+  agencyId: string,
+  agencyDetails: Partial<Agency>
+) => {
+  const response = await db.agency.update({
+    where: { id: agencyId },
+    data: { ...agencyDetails },
+  })
+  return response
 }
